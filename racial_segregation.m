eab5ea8e8d93a0ee0f190caf12fbadf61942871d
jadhamawi <jad.hamawi@gmail.com>
2017-03-29 22:54:05 -0400

    racial segregation model, basic is finalized

--- racial_segregation.m
+++ racial_segregation.m
@@ -2,10 +2,8 @@
 % Racial Segregation Model Attempt
 
 % size of grid
-n = 100;
+n = 10;
 
-% number of agents
-S = 0.9*n^2;
 % proportion of vacancies
 vacanicies_proportion = 0.1;
 
@@ -15,8 +13,9 @@ T = 3;
 % colors
 map = [0 0 0; 1 0 0; 0 1 0; 0 0 1];
 
-% racial preference parameter
-rho = 0.4;
+% racial dispreference parameter
+% minimum fraction of friends that I need to be satisfied
+F = 4/8;
 
 max_iterations = 10000;
 
@@ -31,6 +30,7 @@ for i=1:n
     end
 end
 
+figure,
 imagesc(z);
 colormap(map);
 axis('off');
@@ -72,8 +72,10 @@ for k=1:max_iterations
                 if i~=n && j~=n && z(i+1,j+1) ~= z(i,j) && z(i+1,j+1) ~= 0
                     not_like_me = not_like_me +1;
                 end
+                
+                friends = total_neighbours - not_like_me;
 
-                if not_like_me/total_neighbours > rho
+                if friends/total_neighbours < F
 
                     new_x = randi(n);
                     new_y = randi(n);
@@ -96,6 +98,10 @@ for k=1:max_iterations
         end
     end
     if number_of_moves(k) == 0
+        disp('number of iterations to convergence: ')
+        disp(k);
+        disp('total number of moves to convergence: ')
+        disp(sum(number_of_moves))
         break;
     end
 end
@@ -106,6 +112,6 @@ colormap(map);
 axis('off');
 
 %% plot number of moves
-x = 1:max_iterations;
+iterant = 1:max_iterations;
 figure,
-plot(log10(x), log10(number_of_moves));
+plot(log10(iterant), log10(number_of_moves));
